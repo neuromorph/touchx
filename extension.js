51a2c945a6e62da6659cca7954b1aabffec16ca5
Evgeny Gorbovoy <jinek@msn.com>
2024-08-08 19:31:46 +0300

    #1 Touch Trace Support Ripples move with the fingers

--- extension.js
+++ extension.js
@@ -25,7 +25,7 @@ export default class TouchXExtension extends Extension {
     }
 
     _onOskBtnClicked(event){
-       
+
         if (event.type() == Clutter.EventType.TOUCH_END ||
              (event.type() == Clutter.EventType.BUTTON_RELEASE && !event.is_pointer_emulated())){
 
@@ -38,7 +38,7 @@ export default class TouchXExtension extends Extension {
                 OsdWindowManager.show(-1, this._oskDisabledIcon, "OSK Disabled");
                 // console.log('Touch mode disabled');
             }
-            else 
+            else
             {
                 this._seat.get_touch_mode = () => true;
                 Main.keyboard._syncEnabled();
@@ -51,7 +51,7 @@ export default class TouchXExtension extends Extension {
 
         }
 
-        return Clutter.EVENT_PROPAGATE;   
+        return Clutter.EVENT_PROPAGATE;
 
     }
 
@@ -106,7 +106,7 @@ export default class TouchXExtension extends Extension {
         this._icon = null;
         this._oskEnabledIcon = null;
         this._oskDisabledIcon = null;
-    
+
     }
 
     _setOsk() {
@@ -124,14 +124,14 @@ export default class TouchXExtension extends Extension {
         }
     }
 
-    _playAnimation(mode, widget, x, y) {
-        
+    _playAnimation(mode, widget, x, y, reanimate = true) {
+
         let rtime = this._rtime;
         if (widget._stage === undefined)
             throw new Error('Touch X: Widget not added to stage for '+mode);
 
         if (mode == 'touch') {
-            const ripples = widget
+            const ripples = widget;
             ripples._x = x;
             ripples._y = y;
 
@@ -139,33 +139,72 @@ export default class TouchXExtension extends Extension {
             ripples._stage.set_child_above_sibling(ripples._ripple2, ripples._ripple1);
             ripples._stage.set_child_above_sibling(ripples._ripple3, ripples._ripple2);
 
-            // Show three concentric ripples expanding outwards; the exact parameters were found  
-            // by trial and error, so don't look for them to make perfect sense mathematically.
-            // rtime (1 to 20) allows to customize ripple duration.
-            //                                    delay     time        scale opacity => scale
-            ripples._animRipple(ripples._ripple1, 0,        60*rtime,   0.2,   1.0,      1.15);
-            ripples._animRipple(ripples._ripple2, 2*rtime,  70*rtime,   0.0,   0.7,      1.125);
-            ripples._animRipple(ripples._ripple3, 3*rtime,  80*rtime,   0.0,   0.3,      1.0);
-
+            if (reanimate) {
+                // Show three concentric ripples expanding outwards; the exact parameters were found
+                // by trial and error, so don't look for them to make perfect sense mathematically.
+                // rtime (1 to 20) allows to customize ripple duration.
+                //                                    delay     time        scale opacity => scale
+                ripples._animRipple(ripples._ripple1, 0,        60*rtime,   0.2,   1.0,      1.15);
+                ripples._animRipple(ripples._ripple2, 2*rtime,  70*rtime,   0.0,   0.7,      1.125);
+                ripples._animRipple(ripples._ripple3, 3*rtime,  80*rtime,   0.0,   0.3,      1.0);
+            } else {
+                ripples._ripple1.set_position(x, y);
+                ripples._ripple2.set_position(x, y);
+                ripples._ripple3.set_position(x, y);
+            }
         }
 
     }
 
-    _show(mode, x, y) {
+    _show(mode, x, y, touchId) {
 
         if (!this._rippList)
             return;
 
         if (mode == 'touch') {
             for (const ripples of this._rippList) {
-                if (!ripples._ripple3.visible) {
-                    this._playAnimation(mode, ripples, x, y);
-                    return;
-                }            
-            }
-            this._playAnimation(mode, this._rippList[0], x, y);
-        }
+                if (!ripples._touchId) {
+                    ripples._touchId = touchId;
 
+                    this._playAnimation(mode, ripples, x, y, true);
+                    return;
+                }
+            }
+            throw new Error('Touch X: No free ripples for ' + touchId);
+        }
+    }
+
+    _updateRipplePosition(x, y, touchId) {
+
+        if (!this._rippList)
+            return;
+
+        /*console.log(`update: ${touchId}`);*/
+
+        for (const ripples of this._rippList) {
+            if (ripples._touchId === touchId) {
+                this._playAnimation('touch', ripples, x, y , false);
+                return;
+            }
+        }
+        throw new Error('Touch X: No ripples for ' + touchId);
+    }
+
+    _hideRipple(touchId) {
+
+        if (!this._rippList)
+            return;
+
+        for (const ripples of this._rippList) {
+            if (ripples._touchId === touchId) {
+                ripples._touchId = null;
+                ripples._ripple1.opacity = 0;
+                ripples._ripple2.opacity = 0;
+                ripples._ripple3.opacity = 0;
+                return;
+            }
+        }
+        throw new Error('Touch X: No ripples for '+touchId);
     }
 
     _styleRipple(){
@@ -179,17 +218,17 @@ export default class TouchXExtension extends Extension {
         const bgred = parseInt(parseFloat(bgcolor[0]) * 255);
         const bggreen = parseInt(parseFloat(bgcolor[1]) * 255);
         const bgblue = parseInt(parseFloat(bgcolor[2]) * 255);
-        
+
         let ripStyle = ` width: ${radius+2}px; height: ${radius+2}px; 
             background-color: rgba(${bgred},${bggreen},${bgblue},0.5); 
             box-shadow: 0 0 2px 2px rgba(${bgred},${bggreen},${bgblue},0.15); 
             border-radius: ${radius+2}px ${radius+2}px ${radius+2}px ${radius+2}px; `;
-        
+
         for (const ripples of this._rippList) {
             ripples._ripple1.style = ripStyle;
             ripples._ripple2.style = ripStyle;
             ripples._ripple3.style = ripStyle;
-        }   
+        }
 
     }
 
@@ -203,17 +242,26 @@ export default class TouchXExtension extends Extension {
                     ripples.addTo(Main.uiGroup);
                     this._rippList.push(ripples);
                 }
-                
+
             }
             this._styleRipple();
 
             if (!this._touchId) {
                 this._touchId = global.stage.connect('captured-event::touch', (actor, event) => {
                     let [x, y] = event.get_coords();
-                    if (event.type() == Clutter.EventType.TOUCH_BEGIN) {    
-                        this._show('touch', x, y);
+                    let touchId = event.get_event_sequence().get_slot().toString();
+
+
+                    if (event.type() == Clutter.EventType.TOUCH_BEGIN) {
+                        this._show('touch', x, y, touchId);
+                    } else if (event.type() == Clutter.EventType.TOUCH_UPDATE) {
+                        this._updateRipplePosition(x, y, touchId);
+                    } else if (event.type() == Clutter.EventType.TOUCH_END) {
+                        this._hideRipple(touchId);
+                    } else if (event.type() == Clutter.EventType.TOUCH_CANCEL) {
+                        this._hideRipple(touchId);
                     }
-                    
+
                     return Clutter.EVENT_PROPAGATE;
                 });
             }
@@ -251,7 +299,7 @@ export default class TouchXExtension extends Extension {
 
 
     disable() {
-        
+
         if (this._touchId){
             global.stage.disconnect(this._touchId);
             this._touchId = null;
@@ -262,10 +310,10 @@ export default class TouchXExtension extends Extension {
                 ripples?.destroy();
             }
             this._rippList = null;
-        }  
+        }
 
         /* this._removePanelOSK():
-        * - disconnect oskBtn, set oskBtnId null 
+        * - disconnect oskBtn, set oskBtnId null
         * - destroy oskBtn, set null
         * - restore seat.get_touch_mode, set _restoreToucMode null
         * - set this._seat, this._icon to null
